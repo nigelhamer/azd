0186bb96c3e77c2dce48b6c6a6a846b05b762987
NIgel Hamer <nigel@hamersmith.co.uk>
2023-07-03 14:46:36 +0100

    First Commit

---
+++ infra/main.tf
@@ -0,0 +1,22 @@
+locals {
+  product-prefix               = "ph"
+}
+
+# Implements a set of methodologies to apply consistent resource naming using the default Microsoft Cloud Adoption Framework for Azure recommendations
+# https://github.com/aztfmod/terraform-provider-azurecaf/blob/main/docs/resources/azurecaf_name.md
+resource "azurecaf_name" "rg_name" {
+  name          = "${local.product-prefix}-${var.service_name}"
+  resource_type = "azurerm_resource_group"
+  suffixes        = [var.environment_name]
+  random_length = 0
+  clean_input   = true
+}
+
+# Deploy resource group
+resource "azurerm_resource_group" "rg" {
+  name     = azurecaf_name.rg_name.result
+  location = var.location
+  // Tag the resource group with the azd environment name
+  // This should also be applied to all resources created in this module
+  tags = { azd-env-name : var.environment_name }
+}
---
+++ infra/variables.tf
@@ -0,0 +1,16 @@
+# Input variables for the module
+
+variable "location" {
+  description = "The supported Azure location where the resource deployed"
+  type        = string
+}
+
+variable "environment_name" {
+  description = "The name of the azd environment to be deployed"
+  type        = string
+}
+
+variable "service_name" {
+  description = "The name of service to be deployed. Used to contruct Azure resource names"
+  type        = string
+}
