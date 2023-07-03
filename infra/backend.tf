5833d18b2bd8e2e18ae77639dfe4d76ff44e640e
Nigel Hamer <nigel@hamersmith.co.uk>
2023-07-03 15:45:23 +0100

    Update backend.tf

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "azurerm" {
-    storage_account_name   = "${RS_STORAGE_ACCOUNT}",
-    container_name         = "${RS_CONTAINER_NAME}",
+    storage_account_name   = "hamersmith",
+    container_name         = "terraform",
     key                    = "azd/azdremotetest.tfstate",
-    resource_group_name    = "${RS_RESOURCE_GROUP}",
-    client_id              = "${AZURE_TENANT_ID}",
-    tenant_id              = "${AZURE_SUBSCRIPTION_ID}"
+    resource_group_name    = "sandbox-rg",
+    client_id              = "de8d9e87-3921-45e8-923a-7bc42a5e1835",
+    tenant_id              = "dbba3d9f-8b34-49b7-878a-d3d0d70056ce"
   }
 }
