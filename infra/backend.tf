3dc62e0adc0ce815e75abb89c6505a164b5fb4c6
Nigel Hamer <nigel@hamersmith.co.uk>
2023-07-04 15:48:40 +0100

    Update backend.tf

--- infra/backend.tf
+++ infra/backend.tf
@@ -4,7 +4,5 @@ terraform {
     container_name         = "terraform",
     key                    = "azd/azdremotetest.tfstate",
     resource_group_name    = "sandbox-rg",
-    client_id              = "f28cacdb-8acf-4d9b-8050-a9e2d6dacdc9",
-    tenant_id              = "dbba3d9f-8b34-49b7-878a-d3d0d70056ce"
   }
 }
