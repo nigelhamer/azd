60c0da7696859355d7bd4e631795540edd7cd904
Nigel Hamer <nigel@hamersmith.co.uk>
2023-07-05 10:52:37 +0100

    Update backend.tf

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,8 +1,6 @@
+# This needs to be pulled from config
+# Check the state backend details
 terraform {
   backend "azurerm" {
-    storage_account_name   = "hamesmith"
-    container_name         = "terraform"
-    key                    = "azd/azdremotetest.tfstate"
-    resource_group_name    = "sandbox-rg"
   }
 }
